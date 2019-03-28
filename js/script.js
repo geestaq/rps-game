b88f5e01ff6a0ae5dfdcc992e9888dfe5ae34f04
Rafał Nowakowski <gee@vp.pl>
2019-03-28 21:48:25 +0100

    Add data-move and logic change

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
-//tablica z opisami ruchow
-const moves = [
-	'papier', 'kamień', 'nożyce'
-];
-//wynik meczu
-let results = [
-	0, //wygrane
-	0 //przegrane
-];
-//do ilu wygranych/przegranych rund gramy
-let settingsRounds = 0;
+//konfiguracja i dane meczu
+let params = {
+	//do ilu wygranych/przegranych rund gramy
+	settingsRounds: 0,
+	//wynik meczu
+	results: [
+		0, //wygrane
+		0 //przegrane
+	],
+	//tablica z opisami ruchow
+	moves: [
+		'papier', 'kamień', 'nożyce'
+	]
+}
 
 let btnNew = document.getElementsByClassName('btn-new-game')[0];
 btnNew.addEventListener('click', function(event) {
 	event.preventDefault();
 	newGame(); //nowa gra
 });
-let btnRock = document.getElementsByClassName('btn-play-rock')[0];
-btnRock.addEventListener('click', function(event) {
-	playerMove(2);
-});
-let btnPaper = document.getElementsByClassName('btn-play-paper')[0];
-btnPaper.addEventListener('click', function(event) {
-	playerMove(1);
-});
-let btnScissors = document.getElementsByClassName('btn-play-scissors')[0];
-btnScissors.addEventListener('click', function(event) {
-	playerMove(3);
-});
+let moveButtons = document.getElementsByClassName('player-move');
+for(let item of moveButtons) {
+	//dodanie obslugi buttonow ruchu gracza
+	item.addEventListener('click', function(event) {
+		playerMove(event.target.getAttribute('data-move'));
+	});
+}
 
 //funkcja tworzy nowa gre
 let newGame = function() {
-	settingsRounds = prompt("Ile rund w meczu?");
+	params.settingsRounds = prompt("Ile rund w meczu?");
 	//walidacja
-	if(isNaN(settingsRounds) || settingsRounds == '' || settingsRounds <= 0) {
+	if(isNaN(params.settingsRounds) || params.settingsRounds == '' || params.settingsRounds <= 0) {
 		alert('Nieprawidłowa liczba');
 		newGame();
 	}
@@ -40,11 +38,14 @@ let newGame = function() {
 	//wlaczenie buttonow gry
 	document.querySelector('.interface').style.display = 'block';
 	//zapisanie rund na ekranie
-	document.querySelector('.settings-rounds').innerHTML = settingsRounds;
+	document.querySelector('.settings-rounds').innerHTML = params.settingsRounds;
 }
 
 //funkcja wykonuje ruchy w rundzie i wyswietla jej wynik
 let playerMove = function(move) {
+	//przypisanie do wykonanego ruchu odpowiedniego numeru
+	move = params.moves.indexOf(move) + 1;
+
 	//ruch komputera
 	let compMove = randomMove();
 
@@ -57,16 +58,16 @@ let playerMove = function(move) {
 
 			result = 'Wygrana!!!';
 			//zapisanie wyniku meczu
-			results[0]++;
+			params.results[0]++;
 		} else {
 			result = 'Przegrana!!!';
 			//zapisanie wyniku meczu
-			results[1]++;
+			params.results[1]++;
 		}
 	}
 	//tekst wyniku
-	result += ': zagrałeś ' + moves[move -1].toUpperCase() +
-		', komputer zagrał ' + moves[compMove -1].toUpperCase();
+	result += ': zagrałeś ' + params.moves[move -1].toUpperCase() +
+		', komputer zagrał ' + params.moves[compMove -1].toUpperCase();
 
 	//wyswietlenie wyniku
 	displayResult(result);
@@ -85,14 +86,14 @@ let displayResult = function(text) {
 	//komunikat
 	document.getElementById('output').innerHTML = text;
 	//wynik meczu
-	document.querySelector('#result .won .value').innerHTML = results[0];
-	document.querySelector('#result .lost .value').innerHTML = results[1];
+	document.querySelector('#result .won .value').innerHTML = params.results[0];
+	document.querySelector('#result .lost .value').innerHTML = params.results[1];
 	//zapisanie rund na ekranie
-	document.querySelector('.settings-rounds').innerHTML = settingsRounds;
+	document.querySelector('.settings-rounds').innerHTML = params.settingsRounds;
 	//czy koniec meczu
-	if(results[0] >= settingsRounds || results[1] >= settingsRounds) {
+	if(params.results[0] >= params.settingsRounds || params.results[1] >= params.settingsRounds) {
 		//koncowy komunikat
-		if(results[0] > results[1]) {
+		if(params.results[0] > params.results[1]) {
 			document.getElementById('output').innerHTML += "<br><br>Koniec meczu!!!<br><br>Wygrana!!! Gratulacje!!!";
 		} else {
 			document.getElementById('output').innerHTML += "<br><br>Koniec meczu!!!<br><br>Przegrana!!! Następnym razem będzie lepiej";
